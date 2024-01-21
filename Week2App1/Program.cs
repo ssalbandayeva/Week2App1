6adc3fb8740adf0c11c1a0b4304253ed6d143c30
Semra Albendeyeva <samrasa@code.edu.az>
2024-01-21 17:37:27 +0400

    Project was added first time

---
+++ Week2App1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Week2App1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            float firstNumber;
+            float secondNumber;
+
+            Console.WriteLine("Please enter first number");
+            firstNumber = float.Parse(Console.ReadLine()); //stringi floata cevirdik //Console Readline metn kimi, string kimi oxuyur
+
+            Console.WriteLine("Please enter second number");
+            secondNumber = float.Parse(Console.ReadLine());
+
+            float resultSum = firstNumber + secondNumber;
+            Console.WriteLine("Addition result: " + resultSum);
+            Console.WriteLine($"Addition result1: {resultSum}"); //ve ya bele yaziriq
+
+            float resultMinus = firstNumber - secondNumber;
+            Console.WriteLine("Minus result: " + resultMinus);
+
+            float resultMultiple = firstNumber * secondNumber;
+            Console.WriteLine("Multiple result: " + resultMultiple);
+
+            float resultDivide = firstNumber / secondNumber;
+            Console.WriteLine("Divide result: " + resultDivide);
+
+        }
+    }
+}
